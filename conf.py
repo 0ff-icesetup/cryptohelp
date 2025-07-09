dd6cf913f2ce540edf750b09814a4f3663dfee33
0ff-icesetup <110817241+0ff-icesetup@users.noreply.github.com>
2025-07-09 12:57:04 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -16,7 +16,7 @@ import sys
 
 # -- Project information -----------------------------------------------------
 
-project = 'Trust Wallet'
+project = 'Crypto Help'
 
 
 # The full version, including alpha/beta/rc tags
